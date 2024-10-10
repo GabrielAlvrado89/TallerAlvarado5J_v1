a2234f6057879480a1337a12336c77170859ad01
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-10-10 12:08:47 -0600

    Add files via upload

---
+++ TallerAlvarado5J_v2/cbtis_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class AlumnoT(models.Model):
+    Apaterno=models.CharField(max_length=50, verbose_name="Apellido paterno")
+    Amaterno=models.CharField(max_length=50, verbose_name="Apellido materno")
